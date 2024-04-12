eece336c2044a1b3251fdd05364b6305c378a6c1
dinixz <148535002+dinixz@users.noreply.github.com>
2024-04-12 14:13:54 +0100

    retry button

--- game.py
+++ game.py
@@ -4,12 +4,6 @@ def possible_moves_player(state:Maze):
     return {'up': state.up(), 'down': state.down(), 'left': state.left(), 'right': state.right(), '-': state}
 
 def get_hint(maze:Maze, final_maze:Maze) -> str:
-    '''
-        Podiamos gerar o final_state desta forma:
-            1- O jogador jogou e atualiza-se o estado
-            2- Geramos logo o final_state usando o melhor algoritmo
-            3- Quando o jogador quiser uma dica já não precisa de se gerar o final_state naquele momento, evitando esperas, basta passá-los para esta funçao
-    '''
     hint = {up_symbol: 'up', down_symbol: 'down', left_symbol: 'left', right_symbol: 'right'}
     next_move = final_maze.maze[maze.cur_line, maze.cur_col]
     return hint.get(next_move)
