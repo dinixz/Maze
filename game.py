dda8097b8c73c4d4b576af76d09f95735ec600c3
dinixz <148535002+dinixz@users.noreply.github.com>
2024-04-12 12:46:08 +0100

    funcioona

--- game.py
+++ game.py
@@ -1,10 +1,9 @@
-from Maze import *
 from algorithms import *
 
 def possible_moves_player(state:Maze):
-    return {'up': state.up(), 'down': state.down(), 'left': state.left(), 'right': state.right()}
+    return {'up': state.up(), 'down': state.down(), 'left': state.left(), 'right': state.right(), '-': state}
 
-def get_hint(state:Maze, final_state:Maze) -> str:
+def get_hint(maze:Maze, final_maze:Maze) -> str:
     '''
         Podiamos gerar o final_state desta forma:
             1- O jogador jogou e atualiza-se o estado
@@ -12,6 +11,21 @@ def get_hint(state:Maze, final_state:Maze) -> str:
             3- Quando o jogador quiser uma dica já não precisa de se gerar o final_state naquele momento, evitando esperas, basta passá-los para esta funçao
     '''
     hint = {up_symbol: 'up', down_symbol: 'down', left_symbol: 'left', right_symbol: 'right'}
-    next_move = final_state.maze[state.cur_line, state.cur_col]
+    next_move = final_maze.maze[maze.cur_line, maze.cur_col]
     return hint.get(next_move)
+
+def game_over(maze:Maze, final_maze:Maze) -> bool: #mudar para ver se a ia resolve, se nao acaba
+    if maze.cur_line != 0:
+        if maze.maze[maze.cur_line - 1, maze.cur_col] != final_maze.maze[maze.cur_line - 1, maze.cur_col] and not maze.maze[maze.cur_line - 1, maze.cur_col] in {None, objetivo}:
+            return True
+    if maze.cur_line != maze.lines - 1:
+        if maze.maze[maze.cur_line + 1, maze.cur_col] != final_maze.maze[maze.cur_line + 1, maze.cur_col] and not maze.maze[maze.cur_line + 1, maze.cur_col] in {None, objetivo}:
+            return True
+    if maze.cur_col != 0:
+        if maze.maze[maze.cur_line, maze.cur_col - 1] != final_maze.maze[maze.cur_line, maze.cur_col - 1] and not maze.maze[maze.cur_line, maze.cur_col - 1] in {None, objetivo}:
+            return True
+    if maze.cur_col != maze.columns - 1:
+        if maze.maze[maze.cur_line, maze.cur_col + 1] != final_maze.maze[maze.cur_line, maze.cur_col + 1] and not maze.maze[maze.cur_line, maze.cur_col + 1] in {None, objetivo}:
+            return True
+    return False
     
--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 import pygame, sys, random
 from algorithms import *
+from game import *
 
 COR_PAREDE = (34, 139, 34) # green color
 COR_CIRCLE = (34, 139, 34)
@@ -17,22 +18,33 @@ blue = (0, 0, 128)
 
 pygame.init()
 
+# carregar as imagens usadas
 pygame_player = pygame.image.load('images/pacman.png')
 pygame_end = pygame.image.load('images/end.png')
 pygame_barrier = pygame.image.load('images/barrier.png')
 pygame_return = pygame.image.load('images/return.png')
 pygame_background = pygame.image.load('images/geometric.jpg')
 
+# dimensões do simbolo de return
 return_symbol = pygame.transform.scale(pygame_return, (50,50))
 
-#janela toda
+# janela toda
 janela_size = (1000, 700) 
 janela = pygame.display.set_mode(janela_size)
 
-#botões
+# fontes
+button_font = pygame.font.SysFont("Arial", 30)
+title_font = pygame.font.Font('freesansbold.ttf' ,40)
+
+# botões
+botao_player = pygame.Rect(400 , 300 , 150, 55)
+botao_ia = pygame.Rect(400, 375, 150, 55)
+
 botao_facil = pygame.Rect(400 , 300 , 150, 55)
 botao_medio = pygame.Rect(400, 375, 150, 55)
 botao_dificil = pygame.Rect(400, 450, 150, 55)
+
+botao_hint = pygame.Rect(900, 600, 80, 55)
 botao_return = return_symbol.get_rect() #cria um retangulo para conter a imagem
 botao_return.center = (25,25) #posiçao na janela
 
@@ -46,31 +58,56 @@ def seta(tela, cell_size, linha, coluna, num): #0-up 1-left 2-down 3-right
 
     tela.blit(superficie_rotacionada,( cell_size*coluna, cell_size*linha)) #desenha a seta
 
-def draw_menu():
-    janela.blit(pygame_background, (0,0))
+def draw_mode(): # escolha do modo de jogo
+    janela.blit(pygame_background, (0,0)) # mete o background
 
-    button_font = pygame.font.SysFont("Arial", 30)
-    texto_facil = button_font.render("FÁCIL", True, COR_LETRA)
-    texto_medio = button_font.render("MÉDIO", True, COR_LETRA)
-    texto_dificil = button_font.render("DIFÍCIL", True, COR_LETRA)
+    #textos dos botões
+    texto_player = button_font.render("PLAYER", True, COR_LETRA)
+    texto_ia = button_font.render("IA", True, COR_LETRA)
 
-    pygame.draw.rect(janela, COR_BOTAO, botao_facil, border_radius=10)
-    pygame.draw.rect(janela, COR_BOTAO, botao_medio, border_radius=10)
-    pygame.draw.rect(janela, COR_BOTAO, botao_dificil, border_radius=10)
+    # retangulos dos botoes
+    pygame.draw.rect(janela, COR_BOTAO, botao_player, border_radius=10)
+    pygame.draw.rect(janela, COR_BOTAO, botao_ia, border_radius=10)
 
-    janela.blit(texto_facil, (botao_facil.x + 30, botao_facil.y + 10))
-    janela.blit(texto_medio, (botao_medio.x +30, botao_medio.y+ 10))
-    janela.blit(texto_dificil, (botao_dificil.x + 30, botao_dificil.y + 10))
+    #desenha os botões
+    janela.blit(texto_player, (botao_player.x + 30, botao_player.y + 10))
+    janela.blit(texto_ia, (botao_ia.x +30, botao_ia.y+ 10))
     
-    title_font = pygame.font.Font('freesansbold.ttf' ,40)
+    # titulo
     text = title_font.render("UNEQUAL MAZE THE CHALLENGE", True, (112, 129, 145))
     janela.blit(text, (150, 100))
     
     pygame.display.flip()
 
-def draw_game(maze:Maze):
-    janela.blit(pygame_background, (0,0))
+def draw_menu(): # escolha da dificuldade do maze
+    janela.blit(pygame_background, (0,0)) # mete o background
+
+    # textos
+    texto_facil = button_font.render("FÁCIL", True, COR_LETRA)
+    texto_medio = button_font.render("MÉDIO", True, COR_LETRA)
+    texto_dificil = button_font.render("DIFÍCIL", True, COR_LETRA)
+
+    # retangulos dos botões
+    pygame.draw.rect(janela, COR_BOTAO, botao_facil, border_radius=10)
+    pygame.draw.rect(janela, COR_BOTAO, botao_medio, border_radius=10)
+    pygame.draw.rect(janela, COR_BOTAO, botao_dificil, border_radius=10)
+
+    # desenha os botões
+    janela.blit(texto_facil, (botao_facil.x + 30, botao_facil.y + 10))
+    janela.blit(texto_medio, (botao_medio.x +30, botao_medio.y+ 10))
+    janela.blit(texto_dificil, (botao_dificil.x + 30, botao_dificil.y + 10))
+    janela.blit(return_symbol, botao_return)
     
+    # titulo
+    text = title_font.render("UNEQUAL MAZE THE CHALLENGE", True, (112, 129, 145))
+    janela.blit(text, (150, 100))
+    
+    pygame.display.flip()
+
+def draw_game(maze:Maze, game_mode:str, direction=None): #desenhar a tela de jogo
+    janela.blit(pygame_background, (0,0)) # mete o background
+    
+    # dimensões do espaço do maze
     cell_size = 600// max(maze.lines, maze.columns)
     tela_size = (cell_size * maze.columns, cell_size * maze.lines)
     tela = pygame.Surface(tela_size) #tela onde vai estar o labirinto
@@ -78,22 +115,34 @@ def draw_game(maze:Maze):
     relogio = pygame.time.Clock()
     relogio.tick(20)
     
+    janela.blit(return_symbol, botao_return) # botão de return
+    
+    if game_mode == 'player':
+        
+        #botao hint
+        text_hint = button_font.render("HINT", True, COR_LETRA)
+        pygame.draw.rect(janela, COR_BOTAO, botao_hint, border_radius=10)
+        janela.blit(text_hint, (botao_hint.x + 8, botao_hint.y + 10))
+        
+        maze = player_move(tela, maze, cell_size, direction) # função para desenhar o maze em si
+        pygame.display.flip()
+        return maze # para podermos controlar o maze atual
+        
+    elif game_mode == 'ia':
+        draw_steps(tela, maze, cell_size) # desenha os passos 1 a 1
+        pygame.display.flip()
+        return
+    
+def draw_maze(tela,  maze, cell_size): # desenha o maze e o maze que se passa é uma np.array
     player = pygame.transform.scale(pygame_player, (cell_size//2, cell_size//2))
     end = pygame.transform.scale(pygame_end, (cell_size//2, cell_size//2))
     barrier = pygame.transform.scale(pygame_barrier, (cell_size//2, cell_size//2))
+    items = {objetivo: end, cur: player, obstaculo: barrier} # dicionario para correspondencia simbolo do tabuleiro e imagem
     
-    janela.blit(return_symbol, botao_return)
-    
-    items = {objetivo: end, cur: player, obstaculo: barrier}
-    
-    draw_steps(tela, maze, cell_size, items)
-    
-    pygame.display.flip()
-
-def draw_maze(tela,  maze:Maze, cell_size, items:dict): #maze é uma np.array
     lines, columns = maze.shape
     for line in range(lines):
         for col in range(columns):
+            # posicionamento de cada cell
             x, y = col * cell_size , line * cell_size
             cell = pygame.Rect( x, y, cell_size, cell_size) # pos_x, pos_y, comprimento, altura
             pygame.draw.rect(tela, (0,0,0), cell, 1) # superficie, cor, item, expessura
@@ -139,15 +188,48 @@ def draw_maze(tela,  maze:Maze, cell_size, items:dict): #maze é uma np.array
 
     janela.blit(tela, ((janela_size[0] - tela.get_size()[0]) // 2,(janela_size[1] - tela.get_size()[1]) // 2))
     
-def draw_steps(tela, maze:Maze, cell_size, items:dict):
+def draw_steps(tela, maze:Maze, cell_size):
     for step in maze.move_history:
-        draw_maze(tela, step, cell_size, items)
+        draw_maze(tela, step, cell_size)
         pygame.display.update()
         time.sleep(0.5)
 
+def pop_final_message(message):
+    final_message_rect = pygame.Rect(0, 0, 500, 200)  # Define as dimensões do retângulo
+    final_message_rect.center = janela.get_rect().center  # Centraliza o retângulo na tela
+    final_text = title_font.render(message, True, COR_LETRA)
+    pygame.draw.rect(janela, COR_BOTAO, final_message_rect, border_radius=50)
+    text_rect = final_text.get_rect(center=final_message_rect.center)  # Centraliza o texto dentro do retângulo
+    janela.blit(final_text, text_rect)
+
+def player_move(tela, maze:Maze, cell_size, direction=None) -> Maze:
+    next_mazes = possible_moves_player(maze)
+    next_maze = next_mazes.get(direction)
+    
+    if next_maze is None:
+        draw_maze(tela, maze.maze, cell_size)
+        return maze
+    else:
+        draw_maze(tela, next_maze.maze, cell_size)
+        return next_maze
+        
+'''
+game_state:
+    mode - estamos na tela de escolher o modo de jogo (ia ou player)
+    menu - estamos na tela de menu a escolher a dificuldade
+    easy_game, medium_game, hard_game - dificuldade do jogo e desenha o jogo
+    playing - depois de ter desenhado o jogo 
+    game_over - apenas no modo player quando nao ha mais movimentos possiveis
+
+game_mode:
+    ia - ver a ia a resolver
+    player - nos jogamos no W,A,S,D
+'''        
+
+
 # Mantém a janela aberta
 running = True
-game_state = 'menu'
+game_state = 'mode'
 while running:
     pygame.display.update()
     for event in pygame.event.get():
@@ -162,24 +244,82 @@ while running:
                     game_state = 'medium_game'
                 elif botao_dificil.collidepoint(event.pos):
                     game_state = 'hard_game'
-            else:
                 if botao_return.collidepoint(event.pos):
+                    game_state = 'mode'
+                    
+            elif game_state == 'mode':
+                if botao_player.collidepoint(event.pos): #player button
                     game_state = 'menu'
+                    game_mode = 'player'
     
-    if game_state == 'menu':
+                elif botao_ia.collidepoint(event.pos): #ia button
+                    game_state = 'menu'
+                    game_mode = 'ia'
+
+            elif game_state == 'playing' and game_mode == 'player':
+                if botao_hint.collidepoint(event.pos):
+                    hint = get_hint(maze, final_maze)
+                    time.sleep(0.1)
+                    maze = draw_game(maze, game_mode, hint)
+                    if maze.is_solved():
+                        time.sleep(1)
+                        pop_final_message('YOU WON')
+                        game_state = 'finished'
+            else:
+                
+                if botao_return.collidepoint(event.pos):
+                    game_state = 'mode'
+                    
+        elif event.type == pygame.KEYDOWN:
+            if game_mode == 'player' and game_state == 'playing':
+                print(final_maze)
+                if event.key == pygame.K_w: #up
+                    maze = draw_game(maze, game_mode, 'up')
+                elif event.key == pygame.K_s: #down
+                    maze = draw_game(maze, game_mode, 'down')
+                elif event.key == pygame.K_a: #left
+                    maze = draw_game(maze, game_mode, 'left')
+                elif event.key == pygame.K_d: #right
+                    maze = draw_game(maze, game_mode, 'right')
+                    
+                if game_over(maze, final_maze):
+                    time.sleep(1)
+                    pop_final_message('GAME OVER')
+                    game_state = 'game_over'
+                if maze.is_solved():
+                    time.sleep(1)
+                    pop_final_message('YOU WON')
+                    game_state = 'finished'
+
+    if game_state == 'mode':
+        draw_mode()
+        
+    elif game_state == 'menu':
         draw_menu()
+        
     elif game_state == 'easy_game':
         maze = random.choice(easy_mazes)
-        maze = a_star_search(maze, distancia_manhattan)
-        draw_game(maze)
-        game_state = 'waiting'
+        final_maze = a_star_search(maze, distancia_manhattan)
+        if game_mode == 'ia':
+            draw_game(final_maze, game_mode)
+        else:
+            draw_game(maze, game_mode)
+        game_state = 'playing'
+        
     elif game_state == 'medium_game':
         maze = random.choice(medium_mazes)
-        maze = a_star_search(maze, distancia_manhattan)
-        draw_game(maze)
-        game_state = 'waiting'
+        final_maze = a_star_search(maze, distancia_manhattan)
+        if game_mode == 'ia':
+            draw_game(final_maze, game_mode)
+        else:
+            draw_game(maze, game_mode)
+        game_state = 'playing'
+        
     elif game_state == 'hard_game':
         maze = random.choice(hard_mazes)
-        maze = a_star_search(maze, distancia_manhattan)
-        draw_game(maze)
-        game_state = 'waiting'
+        final_maze = a_star_search(maze, distancia_manhattan)
+        if game_mode == 'ia':
+            draw_game(final_maze, game_mode)
+        else:
+            draw_game(maze, game_mode)
+        game_state = 'playing'
